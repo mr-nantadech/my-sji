197752deef7b83ff14be3b05b3271364b9a65d8e
Nantadech Chabaram <mr.nantadech@gmail.com>
2024-10-01 23:18:17 +0700

    again

--- my-sji-lib.js
+++ my-sji-lib.js
@@ -2,4 +2,4 @@
 
 //find Department of Employee ID 
 // sample $('employeeProfileJson').getDeptOf('15886')
-$.fn.g=function(e){const p=this,f=p.get().find(t=>t.id===e);return f?f.d||null:null};
+$.fn.getDeptOf=function(e){const p=this,f=p.get().find(t=>t.id===e);return f?f.d||null:null};

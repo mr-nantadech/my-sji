3ec23bc56e68e419ca5868ba013f2f4ec1304b8b
Nantadech Chabaram <mr.nantadech@gmail.com>
2024-10-01 22:37:04 +0700

    try again

--- my-sji-lib.js
+++ my-sji-lib.js
@@ -2,4 +2,11 @@
 
 //find Department of Employee ID 
 // sample $('employeeProfileJson').getDeptOf('15886')
-$.fn.getDeptOf=function(e){const p=this,f=p.find(t=>t.id===e);return f?f.dep||null:null};
+$.fn.getDeptOf = function(id) {
+    const data = this[0]; // Get the first element in the jQuery object
+    if (Array.isArray(data)) {
+        const found = data.find(item => item.id === id);
+        return found ? found.dep || null : null;
+    }
+    return null;
+};

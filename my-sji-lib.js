d02f6c30a6fc5ca87c84f037188f94ac966e6238
Nantadech Chabaram <mr.nantadech@gmail.com>
2024-10-01 23:17:44 +0700

    minifycode

--- my-sji-lib.js
+++ my-sji-lib.js
@@ -2,11 +2,4 @@
 
 //find Department of Employee ID 
 // sample $('employeeProfileJson').getDeptOf('15886')
-$.fn.getDeptOf = function (empId) {
-    const profile = this;
-    const foundProfile = profile.get().find(emp => emp.id === empId);
-    if (foundProfile) {
-        return foundProfile.dep || null;
-    }
-    return null;
-};
+$.fn.g=function(e){const p=this,f=p.get().find(t=>t.id===e);return f?f.d||null:null};
